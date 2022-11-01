b24906a5d52215e64cae50368aac9f2a570e94ba
Willianr7 <rafael.net@hotmail.com.br>
2022-11-01 11:41:27 -0300

    ExercicioJavaScript3

---
+++ vscodenovo.js
@@ -0,0 +1,9 @@
+let peso,nome,altura,data,dados
+
+peso = prompt ("Insira seu peso: ")
+nome = prompt ("Insira seu nome: ")
+altura = prompt ("Insira a sua altura: ")
+data = prompt ("Insira a Data de Internação: ")
+dados = prompt ("Você possui diabetes? ")
+
+alert("O seu peso é: " + peso + "\n O seu nome é: " + nome + "\n A sua altura é: " + altura + "\n A data de internação é: " + data + "\n Você tem diabetes? " + dados)
